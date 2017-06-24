e2173279c7536ea932423f72801607db8e2e5a74
Gaohan <gaohan@sas.upenn.edu>
2017-06-24 13:28:09 -0400

    rewrite matrixop.hpp, transfer from mkl to Eigen

--- aux/matrixop.hpp
+++ aux/matrixop.hpp
@@ -1,18 +1,16 @@
 #ifndef _MATRIXOP_HPP
 #define _MATRIXOP_HPP
 /* module for matrix opeations
- * require C++11
+ * require C++14
  * written based on vector.hpp
  * using external lib: 
- * 	mkl
+ * 	Eigen
  *
  * Gaohan
  */
 
-#define MKL_Complex16 std::complex<double>
 #include <cmath>
-#include <complex>
-#include <mkl.h>
+#include <cassert>
 #include "vector.hpp"
 #include "Eigen/Eigenvalues"
 #include "Eigen/Core"
@@ -21,71 +19,88 @@ namespace matrixop{
 	using namespace Eigen;
 	using namespace std;
 
-	inline void Hdiag(const vector<double>& MatrixToDiag, vector<double>& EigenVal, vector<double>& EigenVec){
-		/* diag a real-symmetric matrix
-		 *
-		 * 	param MatrixToDiag: matrix to be diag, column-major, must have N*N elements
-		 * 	param EignVal, EigenVac: vector to store results
-		 * */ 
-		const int N = static_cast<size_t>(sqrt(MatrixToDiag.size()));
-		Map<MatrixXd> A(MatrixToDiag.data(), N, N);
-		SelfAdjointEigenSolver<MatrixXd> es(A);
-	}
+	template<typename MatrixElementType, typename EigenValType, typename EigenVecType>
+		inline void hdiag(const vector<MatrixElementType>& MatrixToDiag, 
+								vector<EigenValType>& EigenVal, 
+								vector<EigenVecType>& EigenVec)
+		{
+			/* diag a symmetric matrix
+			 *
+			 * 	param MatrixToDiag: matrix to be diag, column-major
+			 * 	param EignVal, EigenVac: vector to store results
+			 * */ 
+			using MatrixType = Matrix<MatrixElementType, Dynamic, Dynamic>;
+			const int N = static_cast<size_t>(sqrt(MatrixToDiag.size()));
+			Map<const MatrixType> A(MatrixToDiag.data(), N, N);
+			SelfAdjointEigenSolver<MatrixType> es(A);
+			EigenVec = vector<EigenVecType>(es.eigenvectors().data(), 
+					es.eigenvectors().data() + es.eigenvectors().size());
+			EigenVal = vector<EigenValType>(es.eigenvalues().data(), 
+					es.eigenvalues().data() + es.eigenvalues().size());
+		}
 
-	inline void hdiag(const vector<double>& MatrixToDiag, vector<double>& EigenVal, vector<double>& EigenVec){
-		/* diag a real-symmetric matrix
-		 *
-		 * 	param MatrixToDiag: matrix to be diag, column-major, must have N*N elements
-		 * 	param EignVal, EigenVac: vector to store results
-		 * */ 
-		vector<double> A(MatrixToDiag);
-		const int N = static_cast<size_t>(sqrt(A.size()));
-		// helpers
-		const char UPLO = 'U', COMPZ = 'V';
-		vector<double> d(N), e(N), tau(N), work(N);
-		vector<int> iwork(N);
-		int lwork, liwork, info;
-		// get tri-diagonal, A = QTQ'
-		lwork = -1;
-		dsytrd(&UPLO, &N, &A[0], &N, &d[0], &e[0], &tau[0], &work[0], &lwork, &info);
-		lwork = static_cast<int>(work[0]);
-		work.resize(lwork);
-		dsytrd(&UPLO, &N, &A[0], &N, &d[0], &e[0], &tau[0], &work[0], &lwork, &info);
-		// generate unitary matrix Q
-		lwork = -1;
-		dorgtr(&UPLO, &N, &A[0], &N, &tau[0], &work[0], &lwork, &info);
-		lwork = static_cast<int>(work[0]);
-		work.resize(lwork);
-		dorgtr(&UPLO, &N, &A[0], &N, &tau[0], &work[0], &lwork, &info);
-		// find eva & A
-		lwork = -1;
-		liwork = -1;
-		dstedc(&COMPZ, &N, &d[0], &e[0], &A[0], &N, &work[0], &lwork, &iwork[0], &liwork, &info);
-		lwork = static_cast<int>(work[0]);
-		work.resize(lwork);
-		liwork = iwork[0];
-		iwork.resize(liwork);
-		dstedc(&COMPZ, &N, &d[0], &e[0], &A[0], &N, &work[0], &lwork, &iwork[0], &liwork, &info);
-		// eva = d
-		EigenVal.swap(d);
-		EigenVec.swap(A);
-	}
+	template<typename MatrixElementType>
+		inline vector<MatrixElementType> _matmat(
+				 const vector<MatrixElementType>& Mat1, 
+				 const vector<MatrixElementType>& Mat2,
+				 const size_t M, const size_t N, const size_t K)
+		{
+			/*  matrix-matrix multiplication, 
+			 *  should not be called by external functions
+			 *
+			 * 	param Mat1: matrix of M * N shape
+			 * 	param Mat2: matrix of N * K shape
+			 * 	param M, N, K: dimensions of matrices
+			 *
+			 * 	return a std::vector storing resulting M * K matrix
+			 * */ 
 
-	inline vector<double> dmv(const vector<double>& m, const vector<double>& v, char TRANS = 'N') {
-		/*  matrix-vector multiplication
-		 *
-		 * 	param m: matrix of M * N shape
-		 * 	param v: vector of N length
-		 * */ 
-		const int N = v.size();
-		const int M = static_cast<int>(m.size() / N);
-		vector<double> rst(M);
-		const double alpha = 1.0, beta = 0.0;
-		const int incx = 1;
-		dgemv(&TRANS, &M, &N, &alpha, &m[0], &N, 
-					&v[0], &incx, &beta, &rst[0], &incx);
-		return rst;
-    }
+			// check size
+			assert(Mat1.size() >= M * N);
+			assert(Mat2.size() >= N * K);
+			// determine return type
+			using MatrixType = Matrix<MatrixElementType, Dynamic, Dynamic>;
+			Map<const MatrixType> m1(Mat1.data(), M, N);
+			Map<const MatrixType> m2(Mat2.data(), N, K);
+			MatrixType rst = m1 * m2;
+			return vector<MatrixElementType>(rst.data(), rst.data() + rst.size());
+		}
+
+	template<typename MatrixElementType>
+		inline vector<MatrixElementType> matmat( 
+				const vector<MatrixElementType>& Mat1, 
+				const vector<MatrixElementType>& Mat2,
+				const size_t N)
+		{
+			/*  matrix-matrix multiplication, 
+			 *
+			 * 	param Mat1: matrix with N cols
+			 * 	param Mat2: matrix with N rows
+			 * 	param N:	col number for Mat1 
+			 *
+			 * 	return a std::vector storing resulting M * K matrix
+			 * */ 
+			const size_t M = static_cast<const size_t>(Mat1.size() / N);
+			const size_t K = static_cast<const size_t>(Mat2.size() / N);
+			return _matmat(Mat1, Mat2, M, N, K);
+		}
+
+	template<typename MatrixElementType>
+		inline vector<MatrixElementType> matvec( 
+				const vector<MatrixElementType>& Mat, 
+				const vector<MatrixElementType>& Vec)
+		{
+			/*  matrix-matrix multiplication, 
+			 *
+			 * 	param Mat: matrix with M * N cols
+			 * 	param Vec: vector with N elements
+			 *
+			 * 	return a std::vector with M elements
+			 * */ 
+			const size_t N = static_cast<const size_t>(Vec.size());
+			const size_t M = static_cast<const size_t>(Mat.size() / N);
+			return _matmat(Mat, Vec, M, N, 1);
+		}
 };
 
 #endif // _MATRIXOP_HPP
