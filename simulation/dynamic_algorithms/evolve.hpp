5671c9ef1f0747ae93da5e0a264c6ac639fb18a1
Gaohan <gaohan@sas.upenn.edu>
2017-06-28 23:18:55 -0400

    removed RK4 from evolve

--- simulation/dynamic_algorithms/evolve.hpp
+++ simulation/dynamic_algorithms/evolve.hpp
@@ -9,7 +9,6 @@ namespace scatter{
 		using forcefunc_t = std::vector<double> (&)(const particle_t&, enumspace::dynamics_mode_enum);
 
 		void velocity_verlet( particle_t& ptcl, enumspace::dynamics_mode_enum mode, forcefunc_t forcefunc );
-		void runge_kutta_4th( particle_t& ptcl, enumspace::dynamics_mode_enum mode, forcefunc_t RK4_g );
 	};
 };
 
