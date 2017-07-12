8a22831f6d6f94619ecb6cb313a19c18e3499d21
Eplistical <gaohan@sas.upenn.edu>
2017-07-12 16:45:58 -0400

    modified tests

--- test/showdat.py
+++ test/showdat.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 import numpy as np
 
-fname = '.test2d.dat'
+fname = 'test2d.final.dat'
 
 with open(fname, 'rb') as f:
-    tmp = np.fromfile(f, np.int64, 2)
+    tmp = np.fromfile(f, np.uint32, 2)
+    Ntraj = tmp[0]
+    dim = tmp[1]
+    job_info = np.fromfile(f, np.uint32, Ntraj)
+    surf_info = np.fromfile(f, np.uint32, Ntraj)
+    r_info = np.fromfile(f, np.float64, Ntraj * dim)
+    r_info = r_info.reshape(Ntraj, dim)
+    p_info = np.fromfile(f, np.float64, Ntraj * dim)
+    p_info = p_info.reshape(Ntraj, dim)
+
 print(tmp)
+print(job_info)
+print(surf_info)
+print(r_info)
+print(p_info)
 
